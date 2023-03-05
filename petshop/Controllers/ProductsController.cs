7e2aefe31f8739bc6b37b7b986261ea58e278442
= <=>
2023-03-05 17:45:09 +0700

    Add project files.

---
+++ petshop/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace petshop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
